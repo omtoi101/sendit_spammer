c89b002c4a8c3b7fda907d6b633ecc298942a182
omtoi101 <83868916+omtoi101@users.noreply.github.com>
2021-08-03 19:35:35 +1000

    Update sendit_spammer.py

--- sendit_spammer.py
+++ sendit_spammer.py
@@ -14,28 +14,28 @@ url = input("sendit url : ")
 
 # ask for message here
 message = input("your message : ")
-
-
-
+# open chromedrive / get url
+driver = webdriver.Chrome("chromedriver.exe")
+driver.get(url)
 # main code
-
+time.sleep(1)
 while True:
         index +=1
         try:
-                # open chromedrive / get url
-                driver = webdriver.Chrome("chromedriver.exe")
-                driver.get(url)
-                time.sleep(2)
+
+                time.sleep(1)
                 # find the buttons and type the message
                 driver.find_element_by_xpath("""/html/body/div/div[3]/div/div[1]/div[2]/form""").click()
                 keyboard.write(message)
                 driver.find_element_by_xpath("""/html/body/div/div[3]/div/div[2]/div/div[2]""").click()
                 time.sleep(1)
-                driver.quit()
-        except:
+                driver.refresh()
+                # tell you how many time you have spammed
+                print("\r" + f"sendit spammed {index} times", end="")
+        except Exception as e:
                 #error exception
+                print(e)
                 print("error")
                 driver.quit()
                 pass
-        # tell you how many time you have spammed
-        print("\r" + f"sendit spammed {index} times", end="")
+
